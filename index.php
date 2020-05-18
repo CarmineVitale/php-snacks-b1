c7b8456be19b432b619040e55a209c5026bdf2f6
CarmineVitale <carmine.vitale90@gmail.com>
2020-05-18 16:30:27 +0200

    Secondo esercizio completato

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
 <!-- Creiamo un array ‘matches’ contenente altri array i quali rappresentano delle partite di basket di un’ipotetica tappa del calendario. 
 Ogni array della partita avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
 Stampiamo a schermo tutte le partite con questo schema:Olimpia Milano - Cantù | 55-60 -->
+<h1>Primo Jsnack</h1>
+<p><strong>Istruzioni:</strong>Creiamo un array ‘matches’ contenente altri array i quali rappresentano delle partite di basket di un’ipotetica tappa del calendario. Ogni array della partita avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.Stampiamo a schermo tutte le partite con questo schema:Olimpia Milano - Cantù | 55-60</p>
 <?php 
 $game = [
     [
@@ -45,7 +47,7 @@ $game = [
 
 ?>
 
-<h1>Risultati prima giornata NBA</h1>
+<h2>Risultati prima giornata NBA</h2>
 <ul>
 <?php for ($i = 0; $i < count($game); $i++) { ?>
 <?php $match = $game[$i]; ?>
@@ -54,6 +56,39 @@ $game = [
             </li>
         <?php  } ?>
 </ul>
+<br>
+<br>
+<br>
+<br>
+<br>
+<h1>Secondo Jsnack</h1>
+<p><strong>Istruzioni:</strong>
+Passare come parametri GET name, mail e age e verificare (cercando i metodi che nonconosciamo nella documentazione) che:
+1. name sia più lungo di 3 caratteri,
+2. che mail contenga un punto e una chiocciola
+3. e che age sia un numero.Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”</p>
+<?php 
+$data = $_GET;
+?>
+
+<h1>Accesso
+<?php 
+if ( empty($data) ) {
+    echo " negato";
+} elseif ( strlen($data['name']) < 4 ) {
+    echo " negato nome troppo corto";
+} elseif ( strpos($data['mail'], '@') === false || strpos($data['mail'], '.') === false ) {
+    echo " negato: La mail non è scritta nel modo corretto";
+
+} elseif ( is_numeric($data['age']) === false ) {
+    echo " negato. Il campo age non è un valore numerico";
+} else {
+    echo " consentito";
+}
+?>
+
+</h1>
+
 
     
 </body>
